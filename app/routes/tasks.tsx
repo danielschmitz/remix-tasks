dd70d6939ac485a9f720a2090f8e26548288d4c6
Daniel Schmitz <danieljfa@gmail.com>
2023-02-15 08:46:34 -0300

    Estrutura básica com picocss

---
+++ app/routes/tasks.tsx
@@ -0,0 +1,26 @@
+import pico from '../styles/pico.min.css'
+import base from '../styles/base.css'
+import { Link, Outlet } from '@remix-run/react';
+
+export function links() {
+    return [
+        { rel: "stylesheet", href: pico },
+        { rel: "stylesheet", href: base },
+    ];
+}
+
+export default function Tasks() {
+    return <><div className='container'>
+        <nav>
+            <ul>
+                <li><h1>Task List</h1></li>
+            </ul>
+            <ul>
+                <li><Link role="button" to='/tasks/create'>New</Link></li>
+            </ul>
+        </nav>
+        <Outlet/>
+    </div>
+    </>
+
+};
